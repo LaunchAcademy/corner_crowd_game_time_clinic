c0f5dc71512afff78e4308a54b424661e3362ede
Eric Kelly <heroiceric@gmail.com>
2014-08-25 15:26:42 -0400

    Display dynamic data in view

--- app.rb
+++ app.rb
@@ -2,5 +2,38 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/leaderboard' do
-  'hello world'
+  scores = [
+    {
+      home_team: "Patriots",
+      away_team: "Broncos",
+      home_score: 7,
+      away_score: 3
+    },
+    {
+      home_team: "Broncos",
+      away_team: "Colts",
+      home_score: 3,
+      away_score: 0
+    },
+    {
+      home_team: "Patriots",
+      away_team: "Colts",
+      home_score: 11,
+      away_score: 7
+    },
+    {
+      home_team: "Steelers",
+      away_team: "Patriots",
+      home_score: 7,
+      away_score: 21
+    }
+  ]
+
+  @teams = [
+    { name: 'Steelers', wins: 3, losses: 2 },
+    { name: 'Patriots', wins: 2, losses: 5 },
+    { name: 'Colts', wins: 6, losses: 8 }
+  ]
+
+  erb :leaderboard
 end
